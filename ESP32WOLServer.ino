ff3950b750141826ab7a5fe93f3fc0c7adfcd491
sfz1wdorf <106619917+sfz1wdorf@users.noreply.github.com>
2024-03-17 16:18:26 +0100

    Add files via upload

---
+++ ESP32WOLServer.ino
@@ -0,0 +1,169 @@
+/*********
+  Base code for the server was taken from 
+  https://randomnerdtutorials.com/esp32-web-server-arduino-ide/ 
+*********/
+
+// Load Wi-Fi library
+#include <WiFi.h>
+
+//for WakeOnLan
+#include <WiFiUdp.h>
+#include <WakeOnLan.h>
+
+//for DynDNS
+#include <EasyDDNS.h>
+#include "HTTPClient.h"
+
+// Replace with your credentials
+const char* ssid = "WIFI-Name";
+const char* password = "WIFI-Password";
+const char* MACAddress = "80:e8:2c:24:08:b2";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+//set up WiFiUDP to send WOL messages
+WiFiUDP UDP;
+WakeOnLan WOL(UDP);
+
+// Variable to store the HTTP request
+String header;
+
+// Timing for handling HTTP
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
+const long timeoutTime = 2000;
+
+void wakeMyPC() {
+    WOL.setRepeat(3, 100);
+    WOL.calculateBroadcastAddress(WiFi.localIP(), WiFi.subnetMask());
+    WOL.sendMagicPacket(MACAddress); // Send Wake On Lan packet with the above MAC address. Default to port 9.
+    // WOL.sendMagicPacket(MACAddress, 7); // Change the port number
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("MAC address of ESP32: ");
+  Serial.println(WiFi.macAddress());
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+/*
+    List of supported DDNS providers:
+    - "duckdns"
+    - "noip"
+    - "dyndns"
+    - "dynu"
+    - "enom"
+    - "all-inkl"
+    - "selfhost.de"
+    - "dyndns.it"
+    - "strato"
+    - "freemyip"
+    - "afraid.org"
+  */
+  EasyDDNS.service("strato");
+
+/*
+    For DDNS Providers where you get a token:
+      Use this: EasyDDNS.client("domain", "token");
+    
+    For DDNS Providers where you get username and password: ( Leave the password field empty "" if not required )
+      Use this: EasyDDNS.client("domain", "username", "password");
+  */
+  EasyDDNS.client("domain", "username", "password"); // Enter your DDNS Domain & Token
+
+  // Get Notified when your IP changes
+  EasyDDNS.onUpdate([&](const char* oldIP, const char* newIP){
+    Serial.print("EasyDDNS - IP Change Detected: ");
+    Serial.println(newIP);
+});
+
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  // Check for new public IP every 30 seconds
+  EasyDDNS.update(30000);
+
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // check if the client asked to turn on PC
+            if (header.indexOf("GET /on") >= 0) {
+              Serial.println("Client requested to turn on the copmuter");
+              wakeMyPC();
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><title>WakeOnLan</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #088404; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>WakeOnLan</h1>");
+
+            client.println("<p><a href=\"/on\"><button class=\"button\">Start Server</button></a></p>");
+
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
